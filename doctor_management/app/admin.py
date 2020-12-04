1c92863a8f29916b112dd96f6cd426bbf8621b73
nicocalli99 <calligaro.nicola@gmail.com>
2020-12-04 11:28:59 +0100

    add admin thing

--- doctor_management/app/admin.py
+++ doctor_management/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Doctor, Patient, Pill, Prescription)
+class ViewAdmin(admin.ModelAdmin):
+    pass
--- doctor_management/app/urls.py
+++ doctor_management/app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import index, signup, patient_information
+from .views import *
 
 urlpatterns = [
     url(r'^$', index, name='index'),
